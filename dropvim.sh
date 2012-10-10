826afd9cd823fcdc65591d7ef106cd4cbb6f6044
Lincoln Sanderson <lincoln.sanderson@paperton.com>
2012-10-10 15:08:26 +0200

    Initial commit. Would be nice to have it autocopy across if the dropvim folder does not exist already in Dropbox, but meh. Future things.

---
+++ dropvim.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+echo "WARNING! This will attempt to delete the following files/folders:"
+echo "~/.vimrc"
+echo "~/.viminfo"
+echo "~/.vim/"
+echo "Please ensure you have moved these to your dropbox folder, or backed them up in a seperate location."
+
+while true; do
+    read -p "Do you wish to continue [y/n]? " yn
+    case $yn in
+        [Yy]* ) break;;
+        [Nn]* ) exit;;
+        * ) echo "Please answer yes or no.";;
+    esac
+done
+
+rm -rf ~/.vim ~/.vimrc ~/.viminfo
+
+ln -s ~/Dropbox/dropvim/.vimrc ~/.vimrc
+ln -s ~/Dropbox/dropvim/.vim ~/.vim
+ln -s ~/Dropbox/dropvim/.viminfo ~/.viminfo
